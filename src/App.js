c995c03571669a88d2819df3b104b02ae8ee5aca
J3SUNG <wptjd6141@naver.com>
2021-01-26 23:05:49 -0800

    added time

--- src/App.js
+++ src/App.js
@@ -3,44 +3,50 @@ import RacingBarChart from "./RacingBarChart";
 import useInterval from "./useInterval";
 import "./App.css";
 
-const getRandomIndex = array => {
+const getRandomIndex = (array) => {
+  // random index 뽑기
   return Math.floor(array.length * Math.random());
 };
 
 function App() {
-  const [iteration, setIteration] = useState(0);
+  const [time, setTime] = useState(0);
   const [start, setStart] = useState(false);
   const [data, setData] = useState([
     {
       name: "alpha",
       value: 10,
-      color: "#f4efd3"
+      color: "#f4efd3",
     },
     {
       name: "beta",
       value: 15,
-      color: "#cccccc"
+      color: "#cccccc",
     },
     {
       name: "charlie",
       value: 20,
-      color: "#c2b0c9"
+      color: "#c2b0c9",
     },
     {
       name: "delta",
       value: 25,
-      color: "#9656a1"
+      color: "#9656a1",
     },
     {
       name: "echo",
       value: 30,
-      color: "#fa697c"
+      color: "#fa697c",
     },
     {
       name: "foxtrot",
       value: 35,
-      color: "#fcc169"
-    }
+      color: "#fcc169",
+    },
+    {
+      name: "test",
+      value: 55,
+      color: "#faaa69",
+    },
   ]);
 
   useInterval(() => {
@@ -51,12 +57,14 @@ function App() {
           index === randomIndex
             ? {
                 ...entry,
-                value: entry.value + 10
+                value: entry.value + 10,
               }
             : entry
         )
       );
-      setIteration(iteration + 1);
+      let time = new Date();
+      console.log(time.getSeconds());
+      setTime(time.getSeconds());
     }
   }, 500);
 
@@ -67,7 +75,7 @@ function App() {
       <button onClick={() => setStart(!start)}>
         {start ? "Stop the race" : "Start the race!"}
       </button>
-      <p>Iteration: {iteration}</p>
+      <p>time: {time}</p>
     </React.Fragment>
   );
 }
